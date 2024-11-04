0f33e931388fa13264f74646b18634285ee1b042
Calvin Hunter <calhunte@ucsc.edu>
2024-11-04 12:57:14 -0800

    L16 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 // todo
+const app = document.querySelector<HTMLDivElement>("#app");
+
+const button = document.createElement("button");
+
+button.addEventListener("click", () => {
+  alert("you clikced the button");
+});
+
+app?.append(button);
